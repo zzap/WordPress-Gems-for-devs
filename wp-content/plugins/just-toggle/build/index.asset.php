73801578fbd4eeb7505690de2e26717a16870623
Milana Cap <milanazzap@gmail.com>
2024-08-26 18:33:31 +0200

    Add bare interactivity block.

---
+++ wp-content/plugins/just-toggle/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '1e3d46340e3bad4f1b65');
---
+++ wp-content/plugins/just-toggle/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '1e7bda9c7295972569bf', 'type' => 'module');

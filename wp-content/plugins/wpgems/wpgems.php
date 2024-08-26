10b895f73e597772de6ff4108c5136b30f13edec
Milana Cap <milanazzap@gmail.com>
2024-08-26 14:51:15 +0200

    Create bare plugin.

---
+++ wp-content/plugins/wpgems/wpgems.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:     WP Gems
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          Community
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wpgems
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wpgems
+ */
+
+// Your code starts here.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
